4eb0a06b953fc8d1b0346a3f4bebd919d4daa931
Christopher Schell <chris@designrangers.com>
2013-10-16 00:51:23 -0600

    Start for homepage prototpye
    Set up a template and changed header and footer files accordingly

---
+++ footer.php
@@ -0,0 +1,36 @@
+</section><!-- Container End -->
+<footer class="row full-width">
+	<div class="large-4 columns">
+		<img src="http://placehold.it/333x230&text=video">
+	</div>
+	<div class="large-4 columns">
+		Blog posts
+	</div>
+		<div class="large-4 columns">
+		Newsletter
+	</div>
+</footer>
+<div class="row full-width">
+	<?php dynamic_sidebar("Footer"); ?>
+</div>
+
+<footer class="row full-width" role="contentinfo">
+	<div class="small-12 large-4 columns">
+		<p>&copy; <?php echo date('Y'); ?>. <?php _e('Crafted on','reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
+	</div>
+	
+	<div class="small-12 large-8 columns">
+		<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list right')); ?>
+	</div>
+</footer>
+
+<?php wp_footer(); ?>
+
+<script>
+	(function($) {
+		$(document).foundation();
+	})(jQuery);
+</script>
+	
+</body>
+</html>
---
+++ page-home.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Template Name: Homepage
+*/
+get_header(); ?>
+	<div class="row">
+	<!-- Row for main content area -->
+		<div class="small-12 large-12 columns" role="main">
+			<section id="cts-hero">
+				<img src="http://placehold.it/1000x400">
+			</section>
+		</div>
+	</div><!-- Row End -->
+	<div class="row">
+		<div class="large-12 columns">
+			<section id="cta-home-about">
+				<h2>Our Story</h2>
+				<p>Lorem ipsum suitcase of courage cycles goff criterium an shut up legs, pedal once nitto paris-brest-paris oude kwaremont. Berg battoowoo greekgreek, rouleur derby col du galibier on your left longo gp ouest france plouay la fleche wallonne, sanchez bottle madone topsport vlaanderen. </p>
+				<p>Schleck hell of the north schleck, the koppenberg virenque geraardsbergen fred gutter.</p>
+				<a class="button">What to expect</a>
+				<a class="button">Coaching packages</a>
+			</section>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-12 columns">
+			<section id="cta-home-stats">
+				<h2>Key stats</h2>
+					<ul class="large-block-grid-4">
+					  	<li><img src="http://placehold.it/248x248"></li>
+					  	<li><img src="http://placehold.it/248x248"></li>
+					  	<li><img src="http://placehold.it/248x248"></li>
+					 	<li><img src="http://placehold.it/248x248"></li>
+					</ul>
+			</section>
+		</div>
+	</div>
+		<div class="row">
+		<div class="large-12 columns">
+			<section id="cta-home-coaches">
+				<h2>Our Coaches</h2>
+				<ul data-orbit>
+				  <li>
+				    <img src="http://placehold.it/1000x300">
+				    <div class="orbit-caption">...</div>
+				  </li>
+				  <li>
+				    <img src="http://placehold.it/1000x300">
+				    <div class="orbit-caption">...</div>
+				  </li>
+				  <li>
+				    <img src="http://placehold.it/1000x300">				    
+				    <div class="orbit-caption">...</div>
+				  </li>
+				</ul>
+			</section>
+		</div>
+	</div>
+<?php get_footer(); ?>
+
+			
